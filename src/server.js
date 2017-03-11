e1a164bc4f47199459b4f44be6c6ca32119345e4
tidjungs <waewprach.s@ku.th>
2017-03-11 17:20:20 +0700

    add auth route

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import Hapi from 'hapi';
 import Knex from './knex';
+import jwt from 'jsonwebtoken';
 
 const server = new Hapi.Server();
 
@@ -25,13 +26,13 @@ server.route({
   path: '/birds',
   method: 'GET',
   handler: (request, reply) => {
-    
+    console.log('sss');
     Knex('birds')
-    .select('name', 'species', 'picture_url')
     .where({ isPublic: true })
-    .then( ( results ) => {
+    .select('name', 'species', 'picture_url')
+    .then(results => {
 
-      if( !results || results.length === 0 ) {
+      if(!results || results.length === 0) {
         reply( {
           error: true,
           errMessage: 'no public bird found',
@@ -49,12 +50,55 @@ server.route({
   }
 });
 
+server.route({
+  path: '/auth',
+  method: 'POST',
+  handler: (request, reply) => {
+
+    const { username, password } = request.payload;
+
+    Knex('users')
+    .where({ username })
+    .select('guid', 'password').then(( [user] ) => {
+
+      if (!user) {
+        reply({
+          error: true,
+          errMessage: 'the specified user was not found'
+        });
+        
+        return;
+      }
+
+      if (user.password === password) {        
+        const token = jwt.sign({
+          username,
+          scope: user.guid,
+        }, 'vZiYpmTzqXMp8PpYXKwqc9ShQ1UhyAfy', {
+          algorithm: 'HS256',
+          expiresIn: '1h',
+        });
+
+        reply({
+          token,
+          scope: user.guid,
+        });
+      } else {
+        reply( 'incorrect password' );
+      }
+
+    }).catch(err => {
+      reply('server-side error');
+    });
+  }
+});
+
 server.start(err => {
   
   if (err) {
     console.error('Error was handled!');
     console.error(err);
   }
-  
+
   console.log(`Server started at ${ server.info.uri }`);
 });
